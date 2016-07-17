0db5be5258b35a2bd1db90c98650c5936ad8c2df
Omri <omritoptix@gmail.com>
2016-07-17 17:47:37 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='EmmaPython',
+      version='1.0',
+      description='Emma python client',
+      author='Greg Ward',
+      author_email='gward@python.net',
+     )
